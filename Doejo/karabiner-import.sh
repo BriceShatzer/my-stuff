61ca5d9d2b497288e4a78e6e537d604198bca9c1
Brice Shatzer <brice.shatzer@gmail.com>
2016-02-15 12:40:45 -0600

    Addd Karabiner settings

---
+++ Doejo/karabiner-import.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.redo_winstyle_no_term 1
+/bin/echo -n .
+$cli set repeat.initial_wait 416
+/bin/echo -n .
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.reverse_apple_optical_scroll 1
+/bin/echo -n .
+$cli set remap.pclikehomeend 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.pclikecontrolarrow_except_vm 1
+/bin/echo -n .
+/bin/echo
