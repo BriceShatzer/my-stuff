39111f39bcfaead2ef587d2461fabf2319a68471
Brice Shatzer <bshatzer@kinja.com>
2020-03-30 14:32:14 -0500

    updated "twitter adv search link" tampermonkey script

--- Tamper-Monkey-Scripts/Twitter-Advance_Search_Link.js
+++ Tamper-Monkey-Scripts/Twitter-Advance_Search_Link.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Advance Search Link
 // @namespace    http://twitter.com/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       http://github.com/BriceShatzer
 // @match        https://twitter.com/*
@@ -11,18 +11,21 @@
 (function() {
     'use strict';
     window.onload = () => {
-        let nav = document.getElementsByTagName('nav')[0];
-        const link = document.createElement('a');
-        link.style.textAlign = 'center'
-        link.style.fontWeight = '700';
-        link.style.color = 'rgba(29,161,242,1.00)';
-        link.style.textDecoration = 'none';
-        link.href = 'https://twitter.com/search-advanced';
-        link.innerHTML = '🔍 Advance Search';
-        nav.insertBefore(link, nav.firstChild);
+        didWeGetNavYet(document.getElementsByTagName('nav')[0]);
+
 
         function didWeGetNavYet(value){
-            if (value){ nav = value; }
+            if (value){
+                let nav = value;
+                const link = document.createElement('a');
+                link.style.textAlign = 'center'
+                link.style.fontWeight = '700';
+                link.style.color = 'rgba(29,161,242,1.00)';
+                link.style.textDecoration = 'none';
+                link.href = 'https://twitter.com/search-advanced';
+                link.innerHTML = '🔍 Advance Search';
+                nav.insertBefore(link, nav.firstChild);
+            }
             else { setTimeout(()=>{ didWeGetNavYet(document.getElementsByTagName('nav')[0]); }, 1200);}
         }
     }
