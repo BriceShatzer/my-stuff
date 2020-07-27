41314dc4ae64866dafe54ca060eefbe7589e0184
Brice Shatzer <bshatzer@paypal.com>
2020-07-27 12:29:57 -0500

    updated archive.org scrip to deal with web components

--- Tamper-Monkey-Scripts/archiveOrg-link_to_page_save.js
+++ Tamper-Monkey-Scripts/archiveOrg-link_to_page_save.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Link to Save Page
 // @namespace    https://archive.org
-// @version      0.1
+// @version      0.2
 // @description  Add a link to save a webpage
 // @author       http://briceshatzer.com
 // @match        *.archive.org/*
@@ -9,12 +9,15 @@
 // ==/UserScript==
 
 (function() {
-let el = document.querySelector('.navbar-nav .leftmost').previousElementSibling;
-let li = document.createElement('li');
-    li.className = "dropdown dropdown-ia pull-right";
-    li.style.textAlignment ='middle';
-    li.innerHTML = "<a href='https://web.archive.org/save/'><img src='https://archive.org/web/images/icon_savePage.png' alt='save a webpage' style='height:30px;' /></a>";
+    let targetElement = document.getElementById('topnav').firstElementChild.shadowRoot.querySelector('.topnav').querySelector('primary-nav').shadowRoot.querySelector('.upload');
+    let link = document.createElement('span');
 
-    el.insertAdjacentElement('afterend',li);
+    link.style.float = 'right';
+    link.style.marginTop = '1rem';
+    link.style.transition = 'filter 250ms';
+    link.innerHTML = "<a href='https://web.archive.org/save/'><img src='https://archive.org/web/images/icon_savePage.png' alt='save a webpage' style='height:30px;' /></a>";
+    link.onmouseover = ()=>{link.style.filter = 'hue-rotate(180deg)'}
+    link.onmouseout = ()=>{link.style.filter = ''};
+    targetElement.insertAdjacentElement('afterend',link);
 
 })();
