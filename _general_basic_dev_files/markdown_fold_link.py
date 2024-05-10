afdc1b7f04c8fc712f0bc98f36e6b093699f2a70
Brice Shatzer <brice.shatzer@gmail.com>
2024-05-10 14:54:59 -0500

    trying to add sublime plugin lol

---
+++ _general_basic_dev_files/markdown_fold_link.py
@@ -0,0 +1,25 @@
+import sublime
+import sublime_plugin
+
+
+class MarkdownFoldLinksCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.fold(self.view.find_by_selector("meta.link.inline markup.underline.link"))
+
+    def is_enabled(self):
+        return self.view.match_selector(0, "text.html.markdown")
+
+
+class MarkdownLinkFolder(sublime_plugin.ViewEventListener):
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.get("syntax").endswith("Markdown.sublime-syntax")
+
+    def on_load(self):
+        self.view.run_command("markdown_fold_links")
+
+    on_post_save = on_load
+
+
+# https://www.youtube.com/watch?v=lBgDilqulxg
+# put this in the top level of your "User" package => "Preferences: Browse Packages" will open the directory in finder 
