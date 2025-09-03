030643826cc59fbb8661d00fc78c817e0e332baf
Brice Shatzer <brice.shatzer@gmail.com>
2025-09-03 12:36:51 -0500

    rescue time tm script change

--- Tamper-Monkey-Scripts/RescueTime-helper.js
+++ Tamper-Monkey-Scripts/RescueTime-helper.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Add link to hourly view on RescueTime
-// @namespace    https://www.rescuetime.com/dashboard
-// @version      0.1
-// @description  Inject a link to the hourly view on the RescueTime dashboard pages.
+// @name         RescueTime upgrader
+// @namespace    https://www.rescuetime.com/
+// @version      23-8-2025
+// @description  fixes a few things
 // @author       http://github.com/BriceShatzer
 // @match        https://www.rescuetime.com/dashboard/*
 // @match        https://www.rescuetime.com/dashboard?*
@@ -12,6 +12,8 @@
 /* jshint -W097 */
 'use strict';
 
+
+// --- Add link to hourly view on RescueTime
 var path = document.location.pathname
 var link = document.createElement('a');
 var date = '';
@@ -19,6 +21,8 @@ var date = '';
 function parseOutDate(string){
     return string.substring(string.lastIndexOf('/')+1);
 }
+
+
 
 
 if ( /dashboard\/for\/the\/day\/of/.test.path ){
@@ -38,5 +42,44 @@ link.style= 'font-weight:bold;'+
             'border:solid 1px #637787;'
 link.href= 'https://www.rescuetime.com/browse/overview/by/hour/for/the/day/of/'+date;
 
-
 document.querySelector('#page-content-header h1').appendChild(link);
+
+// --- replace non-sense label with chrome plugin name
+/*
+Doesn't seem to work ಠ_ಠ
+
+const targetString = "dhdgffkkebhmkfjojejmpbldmpobfkfo";
+const replacement = "TamperMonkey extension";
+
+function replaceText(node) {
+    alert('fired');
+    if (node.nodeType === Node.TEXT_NODE) {
+        if (node.nodeValue.includes(targetString)) {
+            node.nodeValue = node.nodeValue.replaceAll(targetString, replacement);
+        }
+    } else {
+        console.log('yo');
+        for (let child of node.childNodes) {
+            replaceText(child);
+        }
+    }
+}
+
+// Initial replacement on page load
+replaceText(document.body);
+
+// Set up a MutationObserver to watch for DOM changes
+const observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+        for (let node of mutation.addedNodes) {
+            replaceText(node);
+        }
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
+
+*/
