e355b9b9cafff98c706dc6da24282935d0f6b23e
Brice Shatzer <brice.shatzer@gmail.com>
2025-11-17 20:54:02 -0600

    added tm script for claude

---
+++ Tamper-Monkey-Scripts/Claude_usage-link.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Add Claude usage link
+// @namespace    claude.ai/
+// @version      2025-11-18
+// @description  adds a link to the usage page
+// @author       https://github.com/BriceShatzer
+// @match        https://claude.ai/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=claude.ai
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(()=>{
+    const el = document.querySelector('nav>div');
+    const link = document.createElement('a');
+
+    link.href = "https://claude.ai/settings/usage";
+    link.target = "_blank";
+    link.innerText = "📈";
+    el.append(link);
+    el.style = 'justify-content: space-between';
+    }, 3000);
+
+
+    // Your code here...
+})();
